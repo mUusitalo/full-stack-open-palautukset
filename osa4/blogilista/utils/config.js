7c1d3bc1514d49304968d8710d37142ecdd2a77e
mUusitalo <73437699+mUusitalo@users.noreply.github.com>
2021-08-30 00:02:55 +0300

    Add checks for missing environment variables (doing this because I don't want to spend 5 hours troubleshooting this again)

--- osa4/blogilista/utils/config.js
+++ osa4/blogilista/utils/config.js
@@ -1,4 +1,20 @@
-require('dotenv').config();
+const res = require('dotenv').config();
+
+const REQUIRED_ENVIRONMENT_VARIABLES = ['DB_URL', 'TEST_DB_URL', 'PORT']
+
+function checkConfigValidity() {
+    if (res.error) {
+        throw res.error;
+    }
+
+    for (const variable of REQUIRED_ENVIRONMENT_VARIABLES) {
+        if (!(variable in res.parsed)) {
+            throw new Error(`${variable} is not defined in .env`);
+        }
+    }
+}
+
+checkConfigValidity()
 
 const DB_URL = process.env.NODE_ENV === 'test'
     ? process.env.TEST_DB_URL
