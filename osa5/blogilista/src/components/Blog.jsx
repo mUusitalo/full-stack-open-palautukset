580d7029cf620e5ee4a7dc8c2aef85a3b7898529
mUusitalo <73437699+mUusitalo@users.noreply.github.com>
2022-01-09 21:12:29 +0200

    5.6-5.7

--- osa5/blogilista/src/components/Blog.jsx
+++ osa5/blogilista/src/components/Blog.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+
+  return (
+    <div className='blog'>
+      {blog.title} {blog.author}
+      <button onClick={() => setVisible(!visible)}>{visible ? "hide details" : "show details"}</button>
+      {visible &&
+        <div className='blog-details'>
+          <p>{blog.url}</p>
+          <p>likes {blog.likes} <button>like</button></p>
+          <p>{blog.user.name}</p>
+        </div>
+      }
+      
+    </div>
+  )  
+}
 
 export default Blog
